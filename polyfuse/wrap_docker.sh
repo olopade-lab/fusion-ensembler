dc93c46b8929b4acd16e84a2230f60ff8c7ae6cf
Anna Elizabeth Woodard <awoodard@nd.edu>
2020-08-25 11:02:28 -0500

    Tweak logging in docker wrapper

--- polyfuse/wrap_docker.sh
+++ polyfuse/wrap_docker.sh
@@ -4,8 +4,10 @@ function cleanup {
     DOCKER_CONTAINER=$(cat ${container_id_file})
     if [[ -n "${DOCKER_CONTAINER}" ]]
     then
+        echo cleaning up container ${DOCKER_CONTAINER}
         docker stop "${DOCKER_CONTAINER}"
         docker rm --force  "${DOCKER_CONTAINER}"
+        echo done cleaning up
     fi
     rm -f "${container_id_file}"
 }
@@ -16,7 +18,10 @@ trap cleanup EXIT
 container_id_file=$(mktemp docker_container_.XXXXXX)
 rm ${container_id_file}
 
+echo [$(date +"%T")] starting docker run
 docker run -d --cidfile ${container_id_file} --rm "${@}"
 
 exit_status=$(docker wait $(cat ${container_id_file}))
+echo [$(date +"%T")] docker finished with exit status $exit_status
+
 exit ${exit_status}
